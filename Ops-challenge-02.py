914694e96fc0c295d30ad80a37b50d6b3108dc77
Juan Maldonado <77375345+Juan-bit94@users.noreply.github.com>
2024-01-09 21:36:58 -0800

    Add files via upload

--- Ops-challenge-02.py
+++ Ops-challenge-02.py
@@ -2,12 +2,23 @@
 # Purpose: 
 # Why
 
-# Done: Create a function, calling a function, and passing a variable to the function.
+import datetime
+import os
 
 def my_function(my_list = {5, 4, 3, 2, 1,}):
     for number in my_list:
         print(number)
 
+now  = datetime.datetime.now()
+print(now)
+
+new_date = datetime.date(1996, 12, 11)
+print(new_date)
+
+time_now = time.time()
+print(time_now)
+
+result = os.system('ping -c 2')
 if __name__ == "__main__":
     my_variable = [1, 2, 3, 4, 5]
     my_function(my_variable)
