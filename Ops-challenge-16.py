a64bd43a921c3384659cf8c1ddd6d1bb0bbb6847
Juan Maldonado <77375345+Juan-bit94@users.noreply.github.com>
2024-01-29 20:00:45 -0800

    Update Ops-challenge-16.py

--- Ops-challenge-16.py
+++ Ops-challenge-16.py
@@ -25,10 +25,10 @@ def iterator():
         print(f"An error occurred: {e}")  # This prints a generic error message if an exception occurs
 
 # This function is for Mode 2: Defensive; Password Recognized
-def check_password():
+def checks_password():
     # This prompts the user to input the string to search for and the word list file path
-    user_string = input("Enter the string to search for:\n")
-    filepath = input("Enter your word list file path:\n")
+    user_string = input("Please enter a word to search for:\n")
+    filepath = input("Please enter your word list file path:\n")
 
     try:
         # This opens the specified word list file and again handles potential encoding issues
@@ -38,11 +38,11 @@ def check_password():
 
         # This checks if the user input string is in the word list
         if user_string in word_list:
-            print(f"The string '{user_string}' appeared in the word list.")
+            print(f"The word '{user_string}' appeared in the list.")
         else:
-            print(f"The string '{user_string}' did not appear in the word list.")
+            print(f"The input '{user_string}' did not appear in the list.")
     except FileNotFoundError:
-        print(f"File not found at path: {filepath}")  # Print an error message if the file is not found
+        print(f"Looks like the file was not found at path: {filepath}")  # Print an error message if the file is not found
     except Exception as e:
         print(f"An error occurred: {e}")  # Print a generic error message if an exception occurs
 
@@ -60,7 +60,7 @@ Brute Force Wordlist Attack Tool Menu
         if mode == "1":
             iterator()  # This calls the iterator function for Mode 1
         elif mode == "2":
-            check_password()  # This calls the check_password function for Mode 2
+            checks_password()  # This calls the check_password function for Mode 2
         elif mode == '3':
             break  # This exits the script for Mode 3
         else:
