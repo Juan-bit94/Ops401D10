bb76cf7c993c624f95c83b2b562d57f661734d6f
Juan Maldonado <77375345+Juan-bit94@users.noreply.github.com>
2024-01-29 16:27:35 -0800

    Update Ops-challenge-16.py

--- Ops-challenge-16.py
+++ Ops-challenge-16.py
@@ -1,1 +1,7 @@
+# Script Name:					Ops-challenge-16.py
+# Author: 					    Juan Maldonado
+# Date of latest revision:		1/29/2024
+# Purpose:					    This is a custom tool that performs brute force attacks to better unsetstand
+#                       the types of automation employed by adversaries. 
+
 
